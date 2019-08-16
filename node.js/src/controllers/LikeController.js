21bf86a80aab49e492e25e1259020c1e73c42f4a
André Neves <andreneves3@gmail.com>
2019-08-15 22:47:37 -0300

    Implementação do frontend + Sockets no match

--- node.js/src/controllers/LikeController.js
+++ node.js/src/controllers/LikeController.js
@@ -4,6 +4,7 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
+        console.log(req.connectedUsers);
 
         const { devId } = req.params;
         const { user } = req.headers;
@@ -17,15 +18,26 @@ module.exports = {
             });
         }
 
+        
         if(targetDev.likes.includes(loggedDev._id)) {
-            console.log('DEU MATCH');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            console.log("MATCH");
+            if(loggedSocket) {
+                console.log('user', loggedDev, loggedSocket)
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if(targetSocket) {
+                console.log('target', targetDev, targetSocket)
+                req.io.to(targetSocket).emit('match', loggedDev)
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
         
         await loggedDev.save();
-
-
 
         return res.json(loggedDev);
 
--- node.js/src/server.js
+++ node.js/src/server.js
@@ -4,7 +4,22 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const httpServer = express();
+const server = require('http').Server(httpServer);  
+
+
+
+const io = require('socket.io')(server);
+
+httpServer.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+const connectedUsers = {};
+
 
 const username = 'admin';
 const password = 'admin';
@@ -12,13 +27,34 @@ const dbname = 'omnistack';
 
 mongoose.connect(`mongodb+srv://${username}:${password}@testes-thkpq.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`, {
     useNewUrlParser: true
+}).then(() => {
+    console.log('Mongodb Connected...');
+
+
+
+    httpServer.use(cors());
+
+    httpServer.use(express.json());
+    httpServer.use(routes);
+
+
+    io.on('connection', socket => {
+        /**
+         * @param socket.handshake.query.user - Id do usuário
+         */
+
+        const { user } = socket.handshake.query;
+        connectedUsers[user] = socket.id;
+
+        console.log('user conectado', user);
+    });
+
+
+
+    server.listen(3333);
+
+
+    mongoose.connection.db.collections().then((result) => {
+        console.log(result[0].s.name);
+    });
 });
-
-
-server.use(cors());
-server.use(express.json());
-server.use(routes);
-
-
-
-server.listen(3333);
